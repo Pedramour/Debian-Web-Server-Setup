a8152fdca0fe44534549361f303ddbad6f4c9b39
Ali Jazayeri <jaza.ali@gmail.com>
2015-03-24 15:51:04 +0430

    Update setup.bash

--- setup.bash
+++ setup.bash
@@ -4,6 +4,11 @@ HOSTNAME=${1}
 IP=${2}
 
 
+if [ -z "${HOSTNAME}" ]; then
+  echo "HOSTNAME must be set."
+  exit 0
+fi
+
 if [ -z "${IP}" ]; then
   IP=$(/sbin/ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}')
 fi
@@ -23,12 +28,8 @@ pear install drush/drush
 
 
 mkdir /etc/bind/data
-mkdir -p /websites/webfiles/$HOSTNAME
+mkdir -p /websites/webfiles
 mkdir -p /websites/log/nginx
-
-rm /etc/nginx/sites-enabled/default
-touch /etc/nginx/sites-available/$HOSTNAME
-ln -s /etc/nginx/sites-available/$HOSTNAME /etc/nginx/sites-enabled/$HOSTNAME
 
 
 cat << EOF > /etc/bind/data/db.$HOSTNAME
@@ -90,11 +91,115 @@ zone "0.$(echo $IP | cut -d. -f3).$(echo $IP | cut -d. -f2).$(echo $IP | cut -d.
 EOF
 
 
-# TODO: Config /etc/php5/fpm/php.ini
-# TODO: Config /etc/php5/pool.d/www.conf
+cat << EOF > /etc/nginx/sites-available/$HOSTNAME
+server {
+  listen 80;
+  server_name $HOSTNAME www.$HOSTNAME;
+  root        /websites/webfiles/$HOSTNAME;
+  error_log   /websites/log/nginx/$HOSTNAME-error.log;
+  access_log  /websites/log/nginx/$HOSTNAME-access.log;
+
+  # Enable compression, this will help if you have for instance advagg‎ module
+  # by serving Gzip versions of the files.
+  #gzip_static on;
+
+  location = /favicon.ico {
+    log_not_found off;
+    access_log off;
+  }
+
+  location = /robots.txt {
+    allow all;
+    log_not_found off;
+    access_log off;
+  }
+
+  # This matters if you use drush prior to 5.x
+  # After 5.x backups are stored outside the Drupal install.
+  location = /backup {
+    deny all;
+  }
+
+  # Very rarely should these ever be accessed outside of your lan
+  location ~* \.(txt|log)$ {
+    allow 192.168.0.0/16;
+    deny all;
+  }
+
+  location ~ \..*/.*\.php$ {
+    return 403;
+  }
+
+  # No no for private
+  location ~ ^/sites/.*/private/ {
+  return 403;
+  }
+
+  # Block access to "hidden" files and directories whose names begin with a
+  # period. This includes directories used by version control systems such
+  # as Subversion or Git to store control files.
+  location ~ (^|/)\. {
+    return 403;
+  }
+
+  location / {
+    # This is cool because no php is touched for static content
+    try_files \$uri @rewrite;
+  }
+
+  location @rewrite {
+    # You have 2 options here
+    # For D7 and above:
+    # Clean URLs are handled in drupal_environment_initialize().
+    #rewrite ^ /index.php;
+    # For Drupal 6 and bwlow:
+    # Some modules enforce no slash (/) at the end of the URL
+    # Else this rewrite block wouldn't be needed (GlobalRedirect)
+    rewrite ^/(.*)$ /index.php?q=\$1;
+    # Drupal in a subdirectory
+    #rewrite ^/([^/]*)/(.*)(/?)$ /\$1/index.php?q=\$2&\$args;
+  }
+
+  location ~ \.php$ {
+    fastcgi_split_path_info ^(.+\.php)(/.+)$;
+    #NOTE: You should have "cgi.fix_pathinfo = 0;" in php.ini
+    include fastcgi_params;
+    fastcgi_param SCRIPT_FILENAME \$request_filename;
+    fastcgi_intercept_errors on;
+    fastcgi_pass 127.0.0.0:9000;
+  }
+
+  # Fighting with Styles? This little gem is amazing.
+  # This is for D6
+  #location ~ ^/sites/.*/files/imagecache/ {
+  # This is for D7 and D8
+  location ~ ^/sites/.*/files/styles/ {
+    try_files \$uri @rewrite;
+  }
+
+  location ~* \.(js|css|png|jpg|jpeg|gif|ico)$ {
+    expires max;
+    log_not_found off;
+  }
+}
+EOF
+
+
+replace ";cgi.fix_pathinfo=1" "cgi.fix_pathinfo=0" -- /etc/php5/fpm/php.ini
+replace "listen = /var/run/php5-fpm.sock" "listen = 127.0.0.1:9000" -- /etc/php5/fpm/pool.d/www.conf
+
+
+rm /etc/nginx/sites-enabled/default
+ln -s /etc/nginx/sites-available/$HOSTNAME /etc/nginx/sites-enabled/$HOSTNAME
+
+
+cd /websites/webfiles
+drush dl
+mv drupal-* $HOSTNAME
+cd $HOSTNAME
 
 
 /etc/init.d/hostname.sh
 /etc/init.d/bind9 restart
+/etc/init.d/php5-fpm restart
 /etc/init.d/nginx start
-/etc/init.d/php5-fpm restart 
